b330cd157a3787ebbbb7fcc57357b60393d0ff5e
Devateja7 <devateja.dev@gmail.com>
2019-08-06 17:08:07 +0530

    firsttest

---
+++ test.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+fruits = ["Apple", "Orange", "Banana", "Peach", "Apple",
+          "Banana", "Apple", "Peach", "Apple", "Banana"]
+
+ctr = Counter(fruits)
+print(ctr)
